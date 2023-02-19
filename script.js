1184c48e677a3a1887e9223fc9b32b9fb53be826
Ari <52755963+ariyoujustcode@users.noreply.github.com>
2023-02-19 10:20:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
+
+function openSocial() {
+  document.getElementById("mySocial").style.display = "block";
+}
+
+function closeSocial() {
+  document.getElementById("mySocial").style.display = "none";
+}
